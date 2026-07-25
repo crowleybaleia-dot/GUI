ebc3c6c7dd4889117d7fe3c99acebf0ab520c71f
soucrowley <crowleybaleia@gmail.com>
2026-07-25 14:58:19 -0300

    Update and rename vanta2@.lua to vanta1@.lua

--- vanta1@.lua
+++ vanta1@.lua
@@ -564,7 +564,7 @@ function lib:init(title, subtitle, logoAsset, visibleKey, deletePrevious, logoSi
     pillShadow.Color        = Color3.fromRGB(210, 140, 255)  -- roxo claro/lavado
     pillShadow.BlurRadius   = UDim.new(0, 24)
     pillShadow.Spread       = UDim2.fromOffset(6, 8)
-    pillShadow.Offset       = UDim2.fromOffset(8, 0)
+    pillShadow.Offset       = UDim2.fromOffset(0, 0)
     pillShadow.Transparency = 0.2
     pillShadow.ZIndex       = -1
     pillShadow.Parent       = pill
