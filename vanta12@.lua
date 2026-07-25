4e9ec48dd5f186748482ad03639672ce90ff2377
soucrowley <crowleybaleia@gmail.com>
2026-07-25 18:11:15 -0300

    Create vanta12@.lua file

--- vanta12@.lua
+++ vanta12@.lua
@@ -475,10 +475,10 @@ function lib:init(title, subtitle, logoAsset, visibleKey, deletePrevious, logoSi
         })
         local logoShadow = Instance.new("UIShadow")
         logoShadow.Color        = Color3.fromRGB(100, 160, 255)
-        logoShadow.BlurRadius   = UDim.new(0, 24)
-        logoShadow.Spread       = UDim2.fromOffset(6, 8)
+        logoShadow.BlurRadius   = UDim.new(0, 10)
+        logoShadow.Spread       = UDim2.fromOffset(2, 3)
         logoShadow.Offset       = UDim2.fromOffset(0, 0)
-        logoShadow.Transparency = 0.2
+        logoShadow.Transparency = 0.6
         logoShadow.ZIndex       = -1
         logoShadow.Parent       = logoImg
     else
