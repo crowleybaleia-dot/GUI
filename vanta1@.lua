3ed550e6072c00d6beffebb803f2c00922815f04
soucrowley <crowleybaleia@gmail.com>
2026-07-25 14:55:38 -0300

    Update and rename vanta@.lua to vanta1@.lua

--- vanta1@.lua
+++ vanta1@.lua
@@ -565,7 +565,7 @@ function lib:init(title, subtitle, logoAsset, visibleKey, deletePrevious, logoSi
     pillShadow.BlurRadius   = UDim.new(0, 24)
     pillShadow.Spread       = UDim2.fromOffset(6, 8)
     pillShadow.Offset       = UDim2.fromOffset(8, 0)
-    pillShadow.Transparency = 0.15
+    pillShadow.Transparency = 0.35
     pillShadow.ZIndex       = -1
     pillShadow.Parent       = pill
 
